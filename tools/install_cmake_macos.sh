bc09b5becdb7c0143dadf47fff956de0e201af69
Christoph <redrezo@users.noreply.github.com>
2019-02-26 17:06:43 +0100

    working on build

--- tools/install_cmake_macos.sh
+++ tools/install_cmake_macos.sh
@@ -4,8 +4,6 @@ CMAKE=cmake-3.13.0-Darwin-x86_64
 CMAKE_DIR=$TRAVIS_BUILD_DIR/cmake.portable
 CMAKE_PATH=$CMAKE_DIR/CMake.app/Contents/bin
 
-mkdir -p $CMAKE_DIR
-
 mkdir .setup && cd .setup
 wget --no-check-certificate -nv -t10 https://cmake.org/files/v3.13/$CMAKE.tar.gz
 tar -xvf $CMAKE.tar.gz > /dev/null 2> /dev/null
