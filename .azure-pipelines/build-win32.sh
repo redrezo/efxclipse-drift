9b913cbe2bffcf3d1251e7a85eca46738c7c201b
Christoph Caks <ccaks@bestsolution.at>
2019-08-12 12:22:36 +0200

    testing azure pipelines

--- .azure-pipelines/build-win32.sh
+++ .azure-pipelines/build-win32.sh
@@ -3,4 +3,4 @@
 echo "This is Win32 ${CMAKE_BUILD_TYPE}"
 mkdir build && cd build
 cmake -G "Visual Studio 15 2017 Win64" ../native
-msbuild /property:Configuration=${CMAKE_BUILD_TYPE} releng.sln
+# msbuild /property:Configuration=${CMAKE_BUILD_TYPE} releng.sln
